7b8aa27791fe013aea40572a04aeb740a7cdb34f
AliJaffri <97388985+AliJaffri@users.noreply.github.com>
2025-06-15 17:35:20 -0500

    app.py

--- app.py
+++ app.py
@@ -98,7 +98,9 @@ def convert_headline_to_link(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
+import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=81, debug=True, load_dotenv=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
