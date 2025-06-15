db46c5697f7da386517f1496aec34aafbda6728d
AliJaffri <97388985+AliJaffri@users.noreply.github.com>
2025-06-15 18:28:45 -0500

    FinbertSentiment.py

--- sentiment/FinbertSentiment.py
+++ sentiment/FinbertSentiment.py
@@ -1,17 +1,22 @@
 from transformers import pipeline
 from .SentimentAnalysisBase import SentimentAnalysisBase
+import torch
 
-class FinbertSentiment (SentimentAnalysisBase):
+class FinbertSentiment(SentimentAnalysisBase):
 
     def __init__(self):
+        # Ensure compatibility with Streamlit Cloud (CPU-only)
+        device = 0 if torch.cuda.is_available() else -1
 
         self._sentiment_analysis = pipeline(
-            "sentiment-analysis", model="ProsusAI/finbert")
+            "sentiment-analysis",
+            model="ProsusAI/finbert",
+            device=device
+        )
         super().__init__()
 
     def calc_sentiment_score(self):
-
-        self.df['sentiment'] = self.df['title'].apply(
-            self._sentiment_analysis)
+        self.df['sentiment'] = self.df['title'].apply(self._sentiment_analysis)
         self.df['sentiment_score'] = self.df['sentiment'].apply(
-            lambda x: {x[0]['label'] == 'negative': -1, x[0]['label'] == 'positive': 1}.get(True, 0) * x[0]['score'])
+            lambda x: {x[0]['label'] == 'negative': -1, x[0]['label'] == 'positive': 1}.get(True, 0) * x[0]['score']
+        )
