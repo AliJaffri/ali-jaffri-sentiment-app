1f7b8f643d9cdcf20c6bd5d9ee703551fed1bb9f
AliJaffri <97388985+AliJaffri@users.noreply.github.com>
2025-06-17 01:33:24 -0500

    SentimentAnalysisBase.py

--- sentiment/SentimentAnalysisBase.py
+++ sentiment/SentimentAnalysisBase.py
@@ -1,35 +1,51 @@
 import plotly.express as px
 import plotly.graph_objects as go
+import pandas as pd
 
 
-class SentimentAnalysisBase():
+class SentimentAnalysisBase:
 
     def __init__(self):
-        pass
+        self.symbol = None
+        self.df = pd.DataFrame()
 
     def set_symbol(self, symbol):
         self.symbol = symbol
 
-    def set_data(self, df):
-
+    def set_data(self, df: pd.DataFrame):
         self.df = df
 
     def calc_sentiment_score(self):
-        pass
+        # This method is meant to be overridden in child classes
+        raise NotImplementedError("You must override calc_sentiment_score in a subclass")
 
     def get_sentiment_scores(self):
         return self.df
 
-    def calc_sentiment_score(self):
-        pass
-
     def plot_sentiment(self) -> go.Figure:
-
         column = 'sentiment_score'
 
-        df_plot = self.df.drop(
-            self.df[self.df[f'{column}'] == 0].index)
+        if column not in self.df.columns:
+            raise ValueError(f"'{column}' column not found in the DataFrame")
 
-        fig = px.bar(data_frame=df_plot, x=df_plot['Date Time'], y=f'{column}',
-                     title=f"{self.symbol} Hourly Sentiment Scores")
+        df_plot = self.df[self.df[column] != 0].copy()
+        df_plot.sort_values(by='Date Time', inplace=True)
+
+        fig = px.bar(
+            data_frame=df_plot,
+            x='Date Time',
+            y=column,
+            title=f"{self.symbol} Hourly Sentiment Scores",
+            labels={'sentiment_score': 'Sentiment Score'},
+            color=column,
+            color_continuous_scale='RdYlGn'
+        )
+
+        fig.update_layout(
+            xaxis_title='Date Time',
+            yaxis_title='Sentiment Score',
+            title_x=0.5,
+            template='plotly_white'
+        )
+
         return fig
